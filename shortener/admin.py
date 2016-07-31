8e29a54ad16bb5482211363c2de648ba93f2e852
Hadi <h74629@gmail.com>
2016-07-31 12:57:41 +0430

    Make translation optional

--- shortener/admin.py
+++ shortener/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from modeltranslation.admin import TranslationAdmin
+from django.conf import settings
 from shortener.models import ShortLink
 
 def short_link(obj):
@@ -8,9 +8,17 @@ def short_link(obj):
 short_link.short_description = 'Short Link'
 short_link.allow_tags = True
 
-class ShortLinkAdmin(TranslationAdmin):
-    fields = ('title', 'url', short_link, 'views_count', 'submit_time')
-    readonly_fields = (short_link, 'views_count', 'submit_time')
-    list_display = ('title', 'url', short_link, 'views_count', 'submit_time')
+if getattr(settings, 'USE_MODELTRANSLATION', False):
+    from modeltranslation.admin import TranslationAdmin
+    
+    class ShortLinkAdmin(TranslationAdmin):
+        fields = ('title', 'url', short_link, 'views_count', 'submit_time')
+        readonly_fields = (short_link, 'views_count', 'submit_time')
+        list_display = ('title', 'url', short_link, 'views_count', 'submit_time')
+else:
+    class ShortLinkAdmin(admin.ModelAdmin):
+        fields = ('title', 'url', short_link, 'views_count', 'submit_time')
+        readonly_fields = (short_link, 'views_count', 'submit_time')
+        list_display = ('title', 'url', short_link, 'views_count', 'submit_time')
 
 admin.site.register(ShortLink, ShortLinkAdmin)
