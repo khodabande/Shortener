098b351e6e6ae064e6566902090984564e2758fa
Hadi <h74629@gmail.com>
2016-07-31 12:15:21 +0430

    Integrate with setuptools

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="django-shortener",
+    version='0.9',
+    packages=['shortener'],
+    author="Hadi Khodabande",
+    author_email="h74629@gmail.com",
+    description="This is a django application that shortens your urls with a flexible admin and useful statistics.",
+    license="MIT",
+    keywords="django url shortener",
+    url="https://github.com/khodabande/Shortener",
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+)
